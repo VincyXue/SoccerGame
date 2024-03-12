fddbd04dfa20734555c6e59242ea63a6a3e672f2
VincyXue <vincyxue@gmail.com>
2024-03-12 16:41:11 -0400

    move directory to a separate file

--- game.js
+++ game.js
@@ -1,31 +1,3 @@
-const directory = [
-    {
-        question: "Which country hosted the 2018 FIFA World Cup?",
-        answers: ["Brazil", "Germany", "Russia"],
-        correctAnswer: "Russia"
-    },
-    {
-        question: "In soccer, how many players are on the field for each team during a regulation match?",
-        answers: ["8", "11", "10"],
-        correctAnswer: "11"
-    },
-    {
-        question: "What is the term for a situation in which one player scores three goals in a single game?",
-        answers: ["Hat-trick", "Touchdown", "Slam dunk"],
-        correctAnswer: "Hat-trick"
-    },
-    {
-        question: "Which country won the 2020 UEFA European Championship (Euro 2020), which was held in 2021 due to the COVID-19 pandemic?",
-        answers: ["Italy", "France", "Portugal"],
-        correctAnswer: "Italy"
-    },
-    {
-        question: "In which year did the United States host the FIFA World Cup for the first time?",
-        answers: ["1986", "1994", "2002"],
-        correctAnswer: "1994"
-    },
-];
-
 let startButton = document.querySelector('#start');
 let questionPage = document.querySelector('#questionPage');
 let question = document.querySelector('#question');
